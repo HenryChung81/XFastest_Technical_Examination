96dd1d14a91fa51f45b47c1b6a1c4bd2c665eaca
Henry Chung <chungjenhao81@gmail.com>
2022-05-09 16:29:47 +0800

    add pages controller

--- XFastest_Technical_Examination/config/routes.rb
+++ XFastest_Technical_Examination/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   get 'articles/index'
   devise_for :users
   # get 'blogs/index'
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
   end
   
   
-  root to: "blogs#index"
+  root to: "pages#index"
 end
